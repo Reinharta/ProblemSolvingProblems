6759bedafe174b562ddd3079b120f8336b6e0a62
Anina Reinhart <reinharta@outlook.com>
2018-03-20 20:24:09 -0500

    Function version 3

--- problemSolvingProblems.js
+++ problemSolvingProblems.js
@@ -28,11 +28,27 @@ function displayResult (result) {
 
 /* #2 */
 
-function reverseString(text){
-    let resultString="";
-    for (let i= text.length -1;i>=0;i--){
-        resultString += text[i];
-    }
-    return resultString
+// function reverseString(text){
+//     let resultString="";
+//     for (let i= text.length -1;i>=0;i--){
+//         resultString += text[i];
+//     }
+//     return resultString
+// }
+// displayResult(reverseString("hello"));
+
+
+/*#3*/
+function makeLowCaseOnSpace(text){
+    let workingString = text.toLowerCase().split(' ');
+    return workingString;
 }
-displayResult(reverseString("hello"));
+function captilzeFirstLetter(workingString){
+    
+    for(let idx = 0; idx < workingString.length; idx++){
+        workingString[idx] = workingString[idx].charAt(0).toUpperCase() + workingString[idx].substring(1);
+    }
+    return workingString.join(' ');
+}
+
+displayResult(captilzeFirstLetter(makeLowCaseOnSpace("hello i am learning.")))
