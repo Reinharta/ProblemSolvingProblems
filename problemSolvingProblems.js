81e2b224e58ef4d984a925a430f3fd5495894013
Anina Reinhart <reinharta@outlook.com>
2018-03-20 19:48:43 -0500

    Functioning version 2

--- problemSolvingProblems.js
+++ problemSolvingProblems.js
@@ -1,24 +1,38 @@
 "use strict";
-
-function fizzbuzz () {
-    let result = "";
-    for (let i=0; i<101; i++) {
-        if (i%3===0 && i%5===0) {
-            result+= "FizzBuzz\n";
-        }
-        else if (i%3===0) {
-            result+= "Fizz\n";
-        }
-        else if (i%5===0) {
-            result+= "Buzz\n";
-        }
-        else {
-            result+= i + "\n";
-        }
-    }
-    return result 
-}
 function displayResult (result) {
-    return console.log(result);
+     return console.log(result);
+ }
+
+/* #1 */
+// function fizzbuzz () {
+//     let result = "";
+//     for (let i=0; i<101; i++) {
+//         if (i%3===0 && i%5===0) {
+//             result+= "FizzBuzz\n";
+//         }
+//         else if (i%3===0) {
+//             result+= "Fizz\n";
+//         }
+//         else if (i%5===0) {
+//             result+= "Buzz\n";
+//         }
+//         else {
+//             result+= i + "\n";
+//         }
+//     }
+//     return result 
+// }
+
+// displayResult(fizzbuzz());
+
+
+/* #2 */
+
+function reverseString(text){
+    let resultString="";
+    for (let i= text.length -1;i>=0;i--){
+        resultString += text[i];
+    }
+    return resultString
 }
-displayResult(fizzbuzz());
+displayResult(reverseString("hello"));
