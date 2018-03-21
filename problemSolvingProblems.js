046fd98b7209a7b82f7cca0daba5557dba6ba70e
Anina Reinhart <reinharta@outlook.com>
2018-03-20 19:15:13 -0500

    Functioning verson 1

--- problemSolvingProblems.js
+++ problemSolvingProblems.js
@@ -1,2 +1,24 @@
 "use strict";
 
+function fizzbuzz () {
+    let result = "";
+    for (let i=0; i<101; i++) {
+        if (i%3===0 && i%5===0) {
+            result+= "FizzBuzz\n";
+        }
+        else if (i%3===0) {
+            result+= "Fizz\n";
+        }
+        else if (i%5===0) {
+            result+= "Buzz\n";
+        }
+        else {
+            result+= i + "\n";
+        }
+    }
+    return result 
+}
+function displayResult (result) {
+    return console.log(result);
+}
+displayResult(fizzbuzz());
